f5ff0e8be1541bb7a3191ab5e30d39c558a99aa7
Wolyo <93871074+Wolyo-JS@users.noreply.github.com>
2021-12-04 19:45:19 +0400

    Update durum.js

--- Commands/Information/durum.js
+++ Commands/Information/durum.js
@@ -14,7 +14,7 @@ class Durum extends Command {
         let members = message.guild.members.cache.size
         let tag = this.client.users.cache.filter(x => x.username.includes(Guild.Tag)).size + 10
         let online = message.guild.members.cache.filter(m => m.presence.status !== "offline").size
-        message.channel.send("Can " + members + " toplam üyeye sahip. %" + parseInt(((members - rol) / members) * 100) + " kayıtlı, %" + parseInt((rol / members) * 100) + " kayıtsız, %" + parseInt((online / members) * 100) + " çevrim içi ve %" + parseInt((tag / members) * 100) + " taglı.");
+        message.channel.send("Wolyo " + members + " toplam üyeye sahip. %" + parseInt(((members - rol) / members) * 100) + " kayıtlı, %" + parseInt((rol / members) * 100) + " kayıtsız, %" + parseInt((online / members) * 100) + " çevrim içi ve %" + parseInt((tag / members) * 100) + " taglı.");
 
     }
 
